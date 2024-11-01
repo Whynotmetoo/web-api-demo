d65468fe2b87f90befb97420d9fcdf5d6ac082dc
Carson Ye <nicksoncn@foxmail.com>
2024-11-01 15:50:33 -0400

    add SSL cert for SSL connection

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_SSL_CERT'] = os.path.join(os.path.dirname(__file__), 'certs/DigiCertGlobalRootCA.crt.pem')
 
 mysql = MySQL(app)
 
